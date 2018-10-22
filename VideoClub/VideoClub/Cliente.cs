12f3926c92e5972c879305ecb7a644b3e9bd7947
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-22 09:19:51 +0200

    VideoClub

---
+++ VideoClub/VideoClub/Cliente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub
+{
+    class Cliente
+    {
+        private string nombre, apellido;
+        private int id;
+
+        public Cliente()
+        {
+
+        }
+        public Cliente(string nombre, string apellido, int id)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.id = id;
+        }
+        
+        //GET & SET
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string GetApellido()
+        {
+            return apellido;
+        }
+        public void SetApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public static void RegistrarCliente()
+        {
+            Console.WriteLine("Introduzca el nombre:");
+            string Nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca apellidos:");
+            string Apellidos = Console.ReadLine();
+            Console.WriteLine("Introduzca DNI:");
+            string DNI = Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ VideoClub/VideoClub/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menuChoice = 0;
+            do
+            {
+                Console.WriteLine("HOTEL BOUTIQUE");
+                Console.WriteLine("1. Login");
+                Console.WriteLine("2. Registrarse");
+                Console.WriteLine("3. salir");
+                Console.WriteLine("4. Check-out");
+                Console.WriteLine("5. Exit");
+
+                menuChoice = Convert.ToInt32(Console.ReadLine());
+                Menu(menuChoice);
+
+            } while (menuChoice != 3);
+        }
+    }
+}
