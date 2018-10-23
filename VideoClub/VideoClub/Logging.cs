a6590f723d4ac9b95f2e58ff991cd0a0fb213513
Unai Aldekozea <unaizeni@hotmail.com>
2018-10-23 16:51:55 +0200

    Login

--- VideoClub/VideoClub/Logging.cs
+++ VideoClub/VideoClub/Logging.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace VideoClub
 {
@@ -10,13 +13,18 @@ namespace VideoClub
     {
         private int logChoice;
 
+        String connectionString = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+        SqlConnection conexion; 
+        string cadena;
+        SqlCommand comando;
+
         public Logging()
         {
-
+            conexion = new SqlConnection(connectionString);
         }
         public Logging(string nombre, string apellido, int id)
         {
-
+            conexion = new SqlConnection(connectionString);
         }
         public void LogMenu()
         {
@@ -36,7 +44,7 @@ namespace VideoClub
                         break;
 
                     case 2:
-                        Resgister();
+                        //Register();
                         exit = true;
                         break;
 
@@ -48,13 +56,29 @@ namespace VideoClub
 
             } while (exit == false);
         }
+        //Vamos a crear un logging para eso pedimos email y contraseña
 
-        private bool Log(string user, string password)
+        private void Log()
         {
-            Console.WriteLine("Escribe usuario");
-            user = Console.ReadLine();
+            Console.WriteLine("Escribe email");
+            string email = Console.ReadLine();
             Console.WriteLine("Escribe contraseña");
-            password = Console.ReadLine();
+            string contraseña = Console.ReadLine();
+
+            conexion.Open();
+
+            //Comparar datos introducidos con Base de Datos
+            cadena = "SELECT Email, Contraseña FROM Cliente WHERE (Email='"+email+"') AND (Contraseña='"+ contraseña +"')";
+            comando = new SqlCommand(cadena, conexion);
+            SqlDataReader registros= comando.ExecuteReader();
+
+            if (registros.Read())
+            {
+                Console.WriteLine("El usuario existe");
+            }
+            
+            conexion.Close();
+            Console.ReadLine();
 
         }
     }
